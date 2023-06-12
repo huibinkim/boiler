d3b36bdb287a9d00c596e3d3c2a5b2aeeb7c1fe3
huibinkim <94955669+huibinkim@users.noreply.github.com>
2023-06-12 17:20:40 +0900

    change

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 
 //application/x-www-form-urlencoded 를 분석하여 가져옴
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 //application/json 형태의 파일을 분석하여 가져올 수 있게 함.
-app.use(bodyParser.json());
+app.use(express.json());
 
 const { User } = require("./models/User"); //user가져오기
 const mongoose = require("mongoose");
@@ -16,18 +16,17 @@ mongoose
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
     }
   )
   .then(() => console.log("connect"))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!안녕"));
+app.get("/", (req, res) => res.send("Hello World!"));
 app.post("/register", (req, res) => {
   //회원 가입 할때 필요한 정보를 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
 
   const user = new User(req.body);
+  console.log(user);
   //body-parser가 있기 때문에 User안의 req.body의 내용을 객체로 가져올 수 있다.
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
