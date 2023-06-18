14ce6a80f7b33eefd693e72bb718472b52433eee
huibinkim <94955669+huibinkim@users.noreply.github.com>
2023-06-18 16:36:10 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -3,41 +3,20 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const { User } = require('./models/User'); //user가져오기
+const config = require('./config/key');
 app.use(express.urlencoded({ extended: true }));
 //application/json 형태의 파일을 분석하여 가져올 수 있게 함.
 app.use(express.json());
 
 const mongoose = require('mongoose');
 mongoose
-  .connect(
-    'mongodb+srv://abc00127:rlagmlwl7@cluster0.hcc2lq1.mongodb.net/?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+  })
   .then(() => console.log('mongo connect'))
   .catch((err) => console.log(err));
 
-// connect();
-
-// const registerUser = async (req, res) => {
-//   try {
-//     const user = new User(req.body);
-//     const userStatus = await user.save();
-
-//     if (!userStatus) {
-//       const err = new Error('실패');
-//       res.status(400).json({ success: fail, err });
-//     }
-//     res.status(200).json({ success: true });
-//     console.log(userStatus);
-//   } catch (err) {
-//     res.status(500).send(err);
-//     console.log(err);
-//   }
-// };
-
-app.get('/', (req, res) => res.send('hello woolr'));
+app.get('/', (req, res) => res.send('hello'));
 app.post('/register', (req, res) => {
   const user = new User(req.body);
   user.save((err, userInfo) => {
@@ -47,12 +26,6 @@ app.post('/register', (req, res) => {
     });
   });
 });
-// // 회원가입 페이지로 이동
-// router.get('/', (req, res) => {
-//   res.render('register');
-// });
-// // 회원가입
-// router.post('/', registerUser);
 
 app.listen(port, () => {
   console.log('Example app listening on port 5000!');
